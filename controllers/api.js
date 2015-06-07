444879f05343cba08b1a399869523be470076f85
modsky1 <maude.jake@gmail.com>
2015-06-07 05:48:24 -0400

    added database ifs

--- controllers/api.js
+++ controllers/api.js
@@ -22,38 +22,6 @@ var _ = require('lodash');
 
 var BusinessCard = require('../models/BusinessCard');
 
-// exports.postSignup = function(req, res, next) {
-//   req.assert('email', 'Email is not valid').isEmail();
-//   req.assert('password', 'Password must be at least 4 characters long').len(4);
-//   req.assert('confirmPassword', 'Passwords do not match').equals(req.body.password);
-
-//   var errors = req.validationErrors();
-
-//   if (errors) {
-//     req.flash('errors', errors);
-//     return res.redirect('/signup');
-//   }
-
-//   var user = new User({
-//     email: req.body.email,
-//     password: req.body.password
-//   });
-
-//   User.findOne({ email: req.body.email }, function(err, existingUser) {
-//     if (existingUser) {
-//       req.flash('errors', { msg: 'Account with that email address already exists.' });
-//       return res.redirect('/signup');
-//     }
-//     user.save(function(err) {
-//       if (err) return next(err);
-//       req.logIn(user, function(err) {
-//         if (err) return next(err);
-//         res.redirect('/');
-//       });
-//     });
-//   });
-// };
-
 function saveBusinessCard(card){
   console.log(card)
   var bizCard = new BusinessCard(card);
