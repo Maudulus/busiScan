9514756cfe12d0d90d64b8e144b2cee5cce28d8c
modsky1 <maude.jake@gmail.com>
2015-06-07 06:44:15 -0400

    s3 attempt

--- controllers/api.js
+++ controllers/api.js
@@ -22,13 +22,28 @@ var _ = require('lodash');
 
 var BusinessCard = require('../models/BusinessCard');
 
+var s3 = require('s3');
+var client = s3.createClient({
+  maxAsyncS3: 20,     // this is the default
+  s3RetryCount: 3,    // this is the default
+  s3RetryDelay: 1000, // this is the default
+  multipartUploadThreshold: 20971520, // this is the default (20 MB)
+  multipartUploadSize: 15728640, // this is the default (15 MB)
+  s3Options: {
+    accessKeyId: secrets.AWS_ACCESS_KEY,
+    secretAccessKey: secrets.AWS_SECRET_KEY,
+    // any other options are passed to new AWS.S3()
+    // See: http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/Config.html#constructor-property
+  },
+});
+
 function saveBusinessCard(card){
   console.log(card)
   var bizCard = new BusinessCard(card);
   BusinessCard.findOne({ email: card.email }, function(err, existingBizCard) {
     if (existingBizCard) {
       //do something...
-      
+
     }
     bizCard.save(function(err) {
       if (err) return next(err);
@@ -47,7 +62,28 @@ var iodClient= new iod.IODClient('http://api.idolondemand.com','dab574b3-1612-42
 exports.receiveImg = function(req,res) {
   // console.log(req)
 
-  var data= {'file':req.files.image.path, 'mode':'scene_photo'}
+  var params = {
+    localFile: req.files.image.path,
+
+    s3Params: {
+      Bucket: "monsiourbucketz",
+      Key: req.files.image.path,
+      // other options supported by putObject, except Body and ContentLength.
+      // See: http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html#putObject-property
+    },
+  };
+  var uploader = client.uploadFile(params);
+  uploader.on('error', function(err) {
+    console.error("unable to upload:", err.stack);
+  });
+  uploader.on('progress', function() {
+    console.log("progress", uploader.progressMd5Amount,
+              uploader.progressAmount, uploader.progressTotal);
+  });
+  uploader.on('end', function() {
+    console.log("done uploading");
+  });  
+  var data = {'file':req.files.image.path, 'mode':'scene_photo'}
   var callback = function(err,resp,body){
     if (body){
       // console.log(body)
